036e2643b71c07550631eebad89bc8ce868bedd4
AlpoxDev <alpoxdev@gmail.com>
2022-10-31 17:46:22 +0900

    add: Node.js Docker Template

--- node/docker/terraform/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "ecr_repository" {
-  name = var.ecr_repository
-
-}
--- node/docker/terraform/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region  = var.aws_provider.region
-  profile = var.aws_provider.profile
-}
